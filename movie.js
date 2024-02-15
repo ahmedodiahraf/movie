247481f6517766d1715b30efe3abb661b18c1831
ahmedodiahraf <142680786+ahmedodiahraf@users.noreply.github.com>
2024-02-15 16:47:07 +0200

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function(){
+     $('.carousel').carousel();
+     });
+
+
+
+function changeBg(bg, title) {
+     const banner = document.querySelector(".banner")
+     const contents = document.querySelectorAll(".content")
+     banner.style.background = `url("./pics/movies/${bg}")`
+     banner.style.backgroundSize = 'cover'
+     banner.style.backgroundPosition = 'center'
+
+     contents.forEach((content) => {
+          content.classList.remove('active')
+          if(content.classList.contains(title)) {
+               content.classList.add('active')
+          }
+     })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
